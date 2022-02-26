f4db877648f63bc9d5d8f5c4581a1d36490c4911
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2022-02-26 18:16:55 +0000

    Removed debugging

--- src/service/testing_service.py
+++ src/service/testing_service.py
@@ -4,13 +4,10 @@ from sklearn import metrics
 def test_model(model, x_test, y_test):
     y_predicted = model.predict(x_test)
 
-    try:
-        cm = metrics.confusion_matrix(y_test, y_predicted)
-        precision = metrics.precision_score(y_test, y_predicted, average=None)
-        recall = metrics.recall_score(y_test, y_predicted, average=None)
-        f1 = metrics.f1_score(y_test, y_predicted, average=None)
-    except Exception as exc:
-        print(exc)
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    precision = metrics.precision_score(y_test, y_predicted, average=None)
+    recall = metrics.recall_score(y_test, y_predicted, average=None)
+    f1 = metrics.f1_score(y_test, y_predicted, average=None)
 
     output = f"f1 Score: {f1}, Precision: {precision}, Recall: {recall}, Confusion Matrix: {cm}"
 
